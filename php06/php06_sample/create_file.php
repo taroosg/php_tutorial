15a0ed43a3564a6a135dc038bd6ba93eba8ca5f3
taroosg <macchinetta.xyz@gmail.com>
2020-06-14 23:32:23 +0900

    update php06

---
+++ php06/php06_sample/create_file.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include("functions.php");
+check_session_id();
+
+if (
+  !isset($_POST['todo']) || $_POST['todo'] == '' ||
+  !isset($_POST['deadline']) || $_POST['deadline'] == ''
+) {
+  // 項目が入力されていない場合はここでエラーを出力し，以降の処理を中止する
+  echo json_encode(["error_msg" => "no input"]);
+  exit();
+}
+
+// 受け取ったデータを変数に入れる
+$todo = $_POST['todo'];
+$deadline = $_POST['deadline'];
+
+
+// ここからファイルアップロード&DB登録の処理を追加しよう！！！
