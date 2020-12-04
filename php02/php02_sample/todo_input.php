d34e7d458dcf5b9204b591a902cca85ab7d9b0fe
taroosg <root@taroosg.dev>
2020-12-04 16:24:10 +0900

    update sample

--- php02/php02_sample/todo_input.php
+++ php02/php02_sample/todo_input.php
@@ -1,31 +1,26 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>DB連携型todoリスト（編集画面）</title>
+  <title>DB連携型todoリスト（入力画面）</title>
 </head>
 
 <body>
-  <form action="todo_update.php" method="POST">
+  <form>
     <fieldset>
-      <legend>DB連携型todoリスト（編集画面）</legend>
+      <legend>DB連携型todoリスト（入力画面）</legend>
       <a href="todo_read.php">一覧画面</a>
       <div>
-        todo: <input type="text" name="todo">
+        todo: <input type="text">
       </div>
       <div>
-        deadline: <input type="date" name="deadline">
+        deadline: <input type="date">
       </div>
       <div>
         <button>submit</button>
       </div>
-
     </fieldset>
   </form>
 
